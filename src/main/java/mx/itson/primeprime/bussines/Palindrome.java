f6f0754811e8aeb87beaa887898ccd9f2214fb1c
alumnog <alumnog@s921-025.itson.edu.mx>
2025-08-28 11:28:19 -0700

    debug y pruebas

--- src/main/java/mx/itson/primeprime/bussines/Palindrome.java
+++ src/main/java/mx/itson/primeprime/bussines/Palindrome.java
@@ -11,6 +11,26 @@ package mx.itson.primeprime.bussines;
 public class Palindrome {
     
     public static boolean isPalindrome(String value){
+        // Limpia la cadena de texto y le quita espacios 
+        String text = value.replaceAll("\\s","").toLowerCase();
+        // convierte el texto a un array de char
+        char[]letras = text.toCharArray();
+        // determina la longitud del array
+        int count = letras.length;
+        
+        for(char l : letras){
+            if (l != letras [count - 1]){
+                return false;
+            } else{
+                count -= 1;
+                //count = count - 1;
+                if (count < 0){
+                    return true;
+                }
+            }
+        }
         return true;
     }
 }
+
+    
